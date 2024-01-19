0cb4a98c5123412dee00981859e5fdb2677caafc
dilla.arkademi <dilla.arkademi@gmail.com>
2024-01-19 11:18:33 +0700

    initial project

---
+++ server.js
@@ -0,0 +1,86 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const Product = require('./models/productModel')
+const app = express()
+
+
+app.use(express.json())
+
+//routes
+app.get('/', (req, res) => {
+  res.send('Hello NODE API')
+})
+
+app.get('/blog', (req, res) => {
+  res.send('Hello NODE API blog')
+})
+
+app.get('/products', async(req, res) => {
+  try {
+    const products = await Product.find({})
+    res.status(200).json(products)
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({message: error.message})
+  }
+})
+
+app.get('/products/:id', async(req, res) => {
+  try {
+    const {id} = req.params;
+    const product = await Product.findById(id)
+    res.status(200).json(product)
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({message: error.message})
+  }
+})
+
+app.post('/products', async(req, res) => {
+  try {
+    const product = await Product.create(req.body)
+    res.status(200).json(product)
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({message: error.message})
+  }
+})
+
+app.put('/products/:id', async(req, res) => {
+  try {
+    const {id} = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body);
+    // we cannot find any product in database
+    if(!product){
+      return res.status(404).json({message: `cannot find any product with ID ${id}`})
+    }
+    const updatedProduct = await Product.findById(id)
+    res.status(200).json(updatedProduct)
+  } catch (error) {
+    res.status(500).json({message: error.message})
+  }
+})
+
+app.delete('/products/:id', async(req, res) => {
+  try {
+    const {id} = req.params;
+    const product = await Product.findByIdAndDelete(id);
+    if(!product){
+      return res.status(404).json({message: `cannot find any product with ID ${id}`})
+    }
+    res.status(202).json(product)
+  } catch (error) {
+    res.status(500).json({message: error.message})
+  }
+})
+
+
+mongoose.connect('mongodb+srv://dillajetmiko:12345678Admin@dillaapi.wk7rja0.mongodb.net/node-api?retryWrites=true&w=majority')
+.then(() => {
+  console.log('connected to mongodb');
+  app.listen(3000, () => {
+    console.log("Node API is running on port 3000");
+  })
+}).catch((error) => {
+  console.log(error);
+})
